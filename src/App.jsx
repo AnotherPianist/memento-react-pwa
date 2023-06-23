efc39685ee4485ed15875d10b729ee91aa18ab80
ThePianist <cafarimo@outlook.com>
2023-06-23 05:53:44 -0400

    Add game ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import {useState} from "react";
+import shuffle from "./utilities/shuffle";
+import Card from "./components/Card";
+
 function App() {
+  const [cards, setCards] = useState(shuffle);
   return (
-    <div>
+    <div className="grid">
+      {cards.map(card => (<Card key={card.id} image={card.image} selected={false} onClick={() => {}}/>))}
     </div>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+export default function Card({image, selected, onClick}) {
+  return (
+    <div className="card">
+      <div className={selected && 'selected'}>
+        <img src={image} className="card-face" alt=""/>
+        <img src="/assets/fireship.png" className="card-back" alt=""/>
+      </div>
+    </div>
+  );
+}
